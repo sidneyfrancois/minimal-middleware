a377d4bccd6dcc4ff294ab58310360a22a367489
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-23 23:58:08 -0300

    add custom middleware as a lambda function

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using MinimalMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.Use(async (context, next) => {
+   if (context.Request.Method == HttpMethods.Get
+   && context.Request.Query["custom"] == "true") 
+   {
+      context.Response.ContentType = "text/plain";
+      await context.Response.WriteAsync("Custom Middleware \n");
+   }
+   
+   await next();
+});
 
 app.UseMiddleware<QueryStringMiddleware>();
 
