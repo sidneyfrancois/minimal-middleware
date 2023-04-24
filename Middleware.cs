0d340d2e63f863dc250457043998bbaa7107fac4
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-23 23:56:02 -0300

    add simple class middleware and add middleware into pipeline

---
+++ Middleware.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MinimalMiddleware;
+
+public class QueryStringMiddleware
+{
+   private RequestDelegate next;
+
+   public QueryStringMiddleware(RequestDelegate nextDelegate)
+   {
+      next = nextDelegate;
+   }
+
+   public async Task Invoke(HttpContext context)
+   {
+      if (context.Request.Method == HttpMethods.Get
+         && context.Request.Query["custom"] == "true") 
+      {
+         if (!context.Response.HasStarted) 
+         {
+            context.Response.ContentType = "text/plain";
+         }
+
+         await context.Response.WriteAsync("Class-based Middleware \n");
+      }
+      
+      await next(context);
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using MinimalMiddleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+app.UseMiddleware<QueryStringMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
