d24b7658d4d3d43e54e6b8cd9b93d85e0eec92f8
Sidney François <sidney.fvm.dev@outlook.com>
2023-05-04 10:39:56 -0300

    add class-based middleware using options pattern

---
+++ LocationMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace MinimalMiddleware;
+
+public class LocationMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly MessageOptions _options;
+
+    public LocationMiddleware(RequestDelegate nextDelegate, IOptions<MessageOptions> opts)
+    {
+        _next = nextDelegate;
+        _options = opts.Value;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Path == "/location")
+        {
+            await context.Response.WriteAsync($"{_options.CityName}, {_options.CountryName}");
+        }
+        else
+        {
+            await _next(context);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,57 +1,19 @@
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using MinimalMiddleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MessageOptions>(options => 
+{
+   options.CityName = "Albany";
+});
+
 var app = builder.Build();
 
-app.Use(async (context, next) => {
-   await next();
-   await context.Response
-      .WriteAsync($"\nStatus Code: { context.Response.StatusCode}");
-});
-
-app.Use(async (context, next) => {
-   if (context.Request.Path == "/short")
-   {
-      await context.Response
-         .WriteAsync($"Request short circuit middleware");
-   } 
-   else 
-   {
-      await next();
-   }
-});
-
-app.Use(async (context, next) => {
-   if (context.Request.Method == HttpMethods.Get
-   && context.Request.Query["custom"] == "true") 
-   {
-      context.Response.ContentType = "text/plain";
-      await context.Response.WriteAsync("Custom Middleware \n");
-   }
-
-   await next();
-});
-
-
-((IApplicationBuilder) app).Map("/branch", branch => 
-{
-   branch.UseMiddleware<QueryStringMiddleware>();
-
-   branch.Run(async (context) =>
-   {
-      await context.Response.WriteAsync($"Branch middleware");
-   }); 
-});
-
-((IApplicationBuilder)app).Map("/branch-terminal", branch =>
-{
-   branch.Run(new QueryStringMiddleware().Invoke);
-});
-
-app.UseMiddleware<QueryStringMiddleware>();
+app.UseMiddleware<LocationMiddleware>();
 
 app.MapGet("/", () => "Hello World!");
 
