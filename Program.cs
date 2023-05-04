95f1edb5d6ef008cca1ef2a7d740891b30cf9242
Sidney François <sidney.fvm.dev@outlook.com>
2023-05-03 21:34:23 -0300

    add short circuit middleware, not forwarding request

--- Program.cs
+++ Program.cs
@@ -8,7 +8,19 @@ var app = builder.Build();
 app.Use(async (context, next) => {
    await next();
    await context.Response
-   .WriteAsync($"\nStatus Code: { context.Response.StatusCode}");
+      .WriteAsync($"\nStatus Code: { context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) => {
+   if (context.Request.Path == "/short")
+   {
+      await context.Response
+         .WriteAsync($"Request short circuit middleware");
+   } 
+   else 
+   {
+      await next();
+   }
 });
 
 app.Use(async (context, next) => {
